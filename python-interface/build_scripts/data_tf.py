0351f3c36e9bf52f1a2565b34863aff0142a756b
tbsexton <tbsexton@asu.edu>
2020-05-21 16:34:18 -0400

    from xml to all other types

--- python-interface/build_scripts/data_tf.py
+++ python-interface/build_scripts/data_tf.py
@@ -1,10 +1,11 @@
 import dask.bag as db
 import dask.dataframe as dd
-
+from xmlschema import XMLSchema
 import json
 from pathlib import Path
 from typing import Dict
 import argparse
+from toolz.curried import update_in, partial, flip, pipe
 
 
 def get_datapaths(build_dir: Path) -> Dict[str, Path]:
@@ -13,53 +14,107 @@ def get_datapaths(build_dir: Path) -> Dict[str, Path]:
     return {f.name.lower(): f for f in datapath.glob("*/") if f.is_dir()}
 
 
-def load_json(json_file: Path):
-    with json_file.open() as f:
-        return json.load(f)
+def get_schema(build_dir: Path) -> XMLSchema:
+    dp = get_datapaths(build_dir)
+    schemapath = str(next(dp["xml"].glob("**/*.xsd")))
+    return XMLSchema(schemapath)
 
 
-def flatten(record):
-    # TODO Find a better way to flatten the authors without loss
-    if "author" in record["root"]["authors"]:
-        record["root"]["authors"] = [
-            author["name"] for author in record["root"]["authors"]["author"]
-        ]
+def load_xml(schema: XMLSchema, p: Path) -> Dict:
+    assert schema.is_valid(str(p)), f"invalid xml detected! {p}"
+    return schema.to_dict(str(p))
+
+
+# def load_json(json_file: Path):
+#     with json_file.open() as f:
+#         return json.load(f)
+
+
+# def flatten(record):
+#     # TODO Find a better way to flatten the authors without loss
+#     if "author" in record["root"]["authors"]:
+#         record["root"]["authors"] = [
+#             author["name"] for author in record["root"]["authors"]["author"]
+#         ]
+#     else:
+#         record["root"]["authors"] = []
+
+#     return record["root"]
+
+
+def flatten_authors(authdict: Dict):
+    if authdict == None:
+        return []
     else:
-        record["root"]["authors"] = []
-
-    return record["root"]
+        return [auth.get("name") for auth in authdict.get("author", [])]
 
 
-def get_bag(build_dir: Path, base_dtype: str = "json") -> db.Bag:
+def split_keywords(kwstr: str):
+    if kwstr == None:
+        return kwstr
+    else:
+        return kwstr.split("\n")
+
+
+def get_bag(build_dir: Path, base_dtype: str = "xml") -> db.Bag:
     """possible to do any text pre-processing here"""
+
     dtype_path = get_datapaths(build_dir).get(base_dtype)
+    schema = get_schema(build_dir)
+    filepaths = dtype_path.glob(f"**/*.{base_dtype}")
+
+    _update_authors = flip(update_in(func=flatten_authors), ("authors",))
+
+    _update_keywords = lambda d: pipe(
+        d,
+        *[
+            update_in(func=split_keywords, keys=[kw])
+            for kw in (col for col in d.keys() if col.endswith("_keywords"))
+        ],
+    )
+
     return (
-        db.from_sequence(dtype_path.glob(f"*.{base_dtype}")).map(load_json).map(flatten)
+        db.from_sequence(filepaths)
+        .map(partial(load_xml, schema))
+        .map(_update_authors)
+        .map(_update_keywords)
     )
 
 
-def bag_to_df(bag: db.Bag, index_key: str = "cord_uid") -> dd.DataFrame:
-    return get_bag(build_dir).to_dataframe().set_index(index_key)
+def bag_to_df(bag: db.Bag, index_key: str = "g_id") -> dd.DataFrame:
+    return bag.to_dataframe()  # .set_index(index_key)  # TODO wait for official id
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="make parquet from json")
+    from dask.distributed import Client, progress
+
+    client = Client()
+
+    parser = argparse.ArgumentParser(description="build data products from CDCS XML")
     parser.add_argument(
         "-o",
         "--overwrite",
         default=False,
         action="store_true",
-        help="overwrite (possible) existing parquet?",
+        help="overwrite (possible) existing data products?",
     )
     args = parser.parse_args()
-
+    build_dir = Path(__file__).resolve().parents[2] / "build"
     parq_file = Path(__file__).parents[1] / "cord19_cdcs" / "cord19_cdcs.parquet"
+    bag = get_bag(build_dir)
+    df = bag_to_df(bag)
+
     if args.overwrite or not parq_file.exists():
-
-        build_dir = Path(__file__).resolve().parents[2] / "build"
-        bag = get_bag(build_dir)
-        df = bag_to_df(bag)
-
+        print("Writing a parquet for python interface...")
         df.to_parquet(parq_file, storage_options={"has_nulls": True}, engine="pyarrow")
+        print("wrote parquet!")
+    if args.overwrite or not (build_dir / "JSON").exists():
+        print("Writing compressed json...")
+        bag.map(json.dumps).to_textfiles(str(build_dir) + "/JSON/cdcs-export_*.json.gz")
+        print("wrote json!")
+    if args.overwrite or not (build_dir / "CSV").exists():
+        print("Writing CSV files...")
+        df.to_csv(str(build_dir) + "/CSV/cdcs-export-*.csv")
+        print("wrote csv!")
     # print(bag.to_dataframe().isna().sum().compute())
     print("done!")
